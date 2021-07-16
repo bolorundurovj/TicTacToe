0e1f58a96bcf4ea1275ef55d2098a2ef7e8f2f95
Bolorunduro Valiant-Joshua <valiantjoshuabolorunduro@gmail.com>
2021-07-16 10:28:35 +0100

    Update UI

--- src/app/square/square.component.ts
+++ src/app/square/square.component.ts
@@ -4,14 +4,15 @@ import { Component, Input} from '@angular/core';
   selector: 'app-square',
   template: `
     <button *ngIf="!value"> {{value}} </button>
-    <button *ngIf="value == 'X'" style="background-color:teal;"> {{value}} </button>
-    <button *ngIf="value == 'O'" style="background-color:aquamarine;"> {{value}} </button>
+    <button *ngIf="value == 'X'" style="background-color:#75E6DA;"> {{value}} </button>
+    <button *ngIf="value == 'O'" style="background-color:#189AB4;"> {{value}} </button>
   `,
   styles: [`button{
     height: 100%;
     width: 100%;
     font-weight: bold;
     font-size: 200%;
+    border: 1px #05445E solid;
   }`]
 })
 export class SquareComponent {
